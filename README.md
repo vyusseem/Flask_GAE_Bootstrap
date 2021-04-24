d3b362838af0954f9d0eec3b1437d20fe767c466
Vlad <vyusseem@gmail.com>
2021-04-24 16:06:57 -0400

    cleanup

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-# Python3-Flask-SQLAlchemy-Bootsrtap
+# Python3-Flask-Google App Engine-Bootsrtap
 Instructions:
 1. clone the repository and navigate to the directory
 2. Create a virtual environment 
 `$ python3 -m venv venv && source venv/bin/activate`
-2. Install the dependencies 
+3. Install the dependencies 
 `$ pip install -r requirements.txt`
-3. Start the server `$ python main.py`
-4. Go to [http://localhost:8080](http://localhost:8080) 
+4. Setup GAE https://cloud.google.com/appengine/docs/standard/python3/quickstart
+5. Use your Google Cloud user credentials to authenticate with the Cloud SDK and enable local testing with Datastore: `gcloud auth application-default login`
+6. Start the server `$ python main.py`
+7. Go to [http://localhost:8080](http://localhost:8080) 
 
 Website made with:
 - python3
 - flask
-- SQLAlchemy
-- sqlite3
+- GAE
+- Google datastore
 - Bootstrap 
 
 Fubctionality:
--- main.py
+++ main.py
@@ -6,64 +6,64 @@ app = Flask(__name__)
 db = datastore.Client()
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/posts', methods = ['GET'])
+@app.route("/posts", methods = ["GET"])
 def posts():
     """ Display all Posts """
     query = db.query(kind="Post", order=["-date_posted"])
-    return render_template('posts.html', posts = query.fetch())
+    return render_template("posts.html", posts = query.fetch())
 
 
-@app.route('/posts/add', methods = ['GET', 'POST'])
+@app.route("/posts/add", methods = ["GET", "POST"])
 def posts_add():
-    '''
+    """
     GET: Display a form to create Post
     POST: Save new Post
-    '''
-    if request.method == 'POST':
+    """
+    if request.method == "POST":
         post = datastore.Entity(db.key("Post"))
         post.update(
             {
-                "title": request.form['title'],
-                "content": request.form['content'],
-                "author": request.form['author'],
+                "title": request.form["title"],
+                "content": request.form["content"],
+                "author": request.form["author"],
                 "date_posted": datetime.now()
             }
         ) 
         db.put(post)
-        return redirect('/posts')
+        return redirect("/posts")
     else:
-        return render_template('posts_form.html', post=None)
+        return render_template("posts_form.html", post=None)
 
 
-@app.route('/posts/edit/<int:id>', methods = ['POST', 'GET'])
+@app.route("/posts/edit/<int:id>", methods = ["POST", "GET"])
 def posts_edit(id):
-    '''
+    """
     GET: Display a form to edit Post
     POST: Update Post
-    '''
+    """
     post = db.get(db.key("Post", id))
-    if request.method == 'POST':
+    if request.method == "POST":
         post["title"] = request.form["title"]
-        post["author"] = request.form['author']
-        post["content"] = request.form['content']  
+        post["author"] = request.form["author"]
+        post["content"] = request.form["content"]  
         db.put(post)
-        return redirect('/posts')
+        return redirect("/posts")
     else:
-        return render_template('posts_form.html', post = post)
+        return render_template("posts_form.html", post = post)
 
 
-@app.route('/posts/delete/<int:id>', methods = ['GET'])
+@app.route("/posts/delete/<int:id>", methods = ["GET"])
 def posts_delete(id):
-    ''' Delete Post '''
+    """ Delete Post """
     db.delete(db.key("Post", id))
-    return redirect('/posts')
+    return redirect("/posts")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
